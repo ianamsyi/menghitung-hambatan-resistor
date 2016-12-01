1f0f75dac3f24f5cf6d8c34a1faf546b7fd0a056
ianamsyi <ianamsyi19@gmail.com>
2016-12-01 11:05:10 +0700

    Add files via upload

---
+++ HITUNG_RESISTOR.ino
@@ -0,0 +1,25 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  digitalWrite(A0,0);
+  int nilaisensor = analogRead(A0);
+  
+  float tegangan = nilaisensor * (5.0 / 1023.0);
+  Serial.println("NILAI TEGANGAN REFERENCE");
+  Serial.println(tegangan);
+  
+  float Vr2=5.0 - tegangan;
+  Serial.println("NILAI TEGANGAN PADA R2");
+  Serial.println(Vr2);
+  
+  float i =Vr2/ 100.0;
+  
+  float r = tegangan/i;
+  Serial.println("NILAI RESISTOR 2");
+  Serial.println(r);
+   
+}
+
+void loop(){   
+}
